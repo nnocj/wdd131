7b519ba3d0622986c8a7b05557e5ea3fb8060855
commey180 <com23005@byui.edu>
2024-09-03 18:04:46 +0000

    hover effect added

--- scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-const navLink = document.querySelector("#a");
-
-navLink.addEventListener('mouseenter', function() {
-    navLink.style.backgroundColor = '#000000'; // Change color on hover
-  });
-
-  button.addEventListener('mouseleave', function() {
-    navLink.style.backgroundColor = '#3498db'; // Revert color when not hovering
-  });
