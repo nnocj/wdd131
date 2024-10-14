8655713be64326d09c319186219631fa7065c312
commey180 <com23005@byui.edu>
2024-10-14 18:30:42 +0000

    saving works so far

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,10 @@
+/*Find the area of a circle for the following 
+Date: 14/10/2024 */
+function circleArea (radius) {
+    const PI = 3.14;
+    area = radius * radius * PI;
+    return area;
+}
+
+console.log(`Area1: ${circleArea(3)}`);
+console.log(`Area2: ${circleArea(4)}`);
