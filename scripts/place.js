1a9e3562bbc097c084bcf0c5f9e227e2a865e8a7
commey180 <com23005@byui.edu>
2024-10-26 18:06:50 +0000

    completed week03 assignment

---
+++ scripts/place.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const calculateWindChill = function(temperature, windSpeed) {
+        let windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+        return windChill.toFixed(2); // Returning the wind chill rounded to 2 decimal places
+    };
+
+    const temperature = parseFloat(document.querySelector("#temp-value").textContent);
+    const wind_speed = parseFloat(document.querySelector("#wind-value").textContent);
+    const wind_chill = document.querySelector("#chill-value");
+
+    if (temperature <= 10 && wind_speed > 4.8) {
+        wind_chill.textContent = calculateWindChill(temperature, wind_speed);
+    } else {
+        wind_chill.textContent = "N/A";
+    }
+});
